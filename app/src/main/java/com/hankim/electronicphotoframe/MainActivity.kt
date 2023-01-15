12ae541759839c64768896da98a1b2f9aceb99d0
HANKIMKOR <95111895+HANKIMKOR@users.noreply.github.com>
2023-01-15 16:24:59 +0900

    [0] start

---
+++ app/src/main/java/com/hankim/electronicphotoframe/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.hankim.electronicphotoframe
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
